05d1b79bb1489c58956a7fc1b2eb8db349dc76ba
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-06-01 19:27:09 -0600

    fix database init script

--- db/database_init.sh
+++ db/database_init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-for database in $(ls *.sql | xargs cat | grep "Database:" | sed 's/`//g' | awk '{print $3}'); do
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "create database $database";
+for file in $(ls /tmp/*.sql); do
+    filename=$(basename $file)
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "create database ${filename%.*}";
+    mysql -u root -p$MYSQL_ROOT_PASSWORD "${filename%.*}" < "$file"
 done
+
