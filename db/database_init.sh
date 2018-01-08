c84c0b5de9b93cd8cb5fbfddda675076af7fe4a1
Santiago Verdu <santiago.verdu.01@gmail.com>
2018-01-08 07:22:34 -0800

    update db init file

--- db/database_init.sh
+++ db/database_init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+databases=$(mysql -uroot -p$MYSQL_ROOT_PASSWORD -se "show databases" 2>/dev/null)
 for file in $(ls /tmp/*.sql 2>/dev/null); do
     filename=$(basename $file)
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "create database ${filename%.*}";
-    mysql -u root -p$MYSQL_ROOT_PASSWORD "${filename%.*}" < "$file"
+    if [[ -n $( printf '%s\n' "${databases[@]}" | grep -e "^${filename%.*}$" ) ]]; then
+        echo "Database already exists: $file"
+        continue
+    fi
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "create database ${filename%.*}"
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD "${filename%.*}" < "$file"
     echo "Added database: $file."
 done
 
